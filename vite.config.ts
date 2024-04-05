110b2ca5bec8e4cff347a1b7380f60d52cc43733
SuperCheese21 <ethan.shields21@gmail.com>
2024-04-05 12:58:17 -0700

    Configure base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: env.NODE_ENV === "production" ? "/ag-grid-ssrm-poc" : "/",
+  };
 });
