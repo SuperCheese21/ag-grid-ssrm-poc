f9f7171c4fdc4fbcc4e61e9888bfcd53df23dbcc
SuperCheese21 <ethan.shields21@gmail.com>
2024-04-05 13:22:27 -0700

    Removed gh-pages package

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    plugins: [react()],
-    base: env.NODE_ENV === "production" ? "/ag-grid-ssrm-poc" : "/",
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/ag-grid-ssrm-poc/",
 });
